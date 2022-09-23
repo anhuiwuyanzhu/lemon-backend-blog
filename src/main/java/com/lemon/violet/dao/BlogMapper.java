efd3297acc74a1922d221dadafdade0b8a2c17ea
吴彦祖 <anhuiwuyanzhu@Gmail.com>
2022-09-23 21:19:19 +0800

    博客项目接口初始化

--- src/main/java/com/lemon/violet/dao/BlogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lemon.violet.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lemon.violet.pojo.po.Blog;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BlogMapper extends BaseMapper<Blog> {
-}

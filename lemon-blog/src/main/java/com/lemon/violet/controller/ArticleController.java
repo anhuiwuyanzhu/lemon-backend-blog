c8c974d7b73900c37c5a7ef7b587b19aee0b0597
吴彦祖 <anhuiwuyanzhu@Gmail.com>
2022-09-26 15:03:37 +0800

    引入日志、文章列表接口定义

--- lemon-blog/src/main/java/com/lemon/violet/controller/ArticleController.java
+++ lemon-blog/src/main/java/com/lemon/violet/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.lemon.violet.controller;
 import com.lemon.violet.pojo.vo.RespVo;
 import com.lemon.violet.service.ArticleService;
 import com.lemon.violet.service.ArticleTagService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+@Api(tags = "文章相关接口")
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
@@ -19,4 +21,30 @@ public class ArticleController {
     public RespVo test(){
         return RespVo.success(articleService.list());
     }
+
+
+    @GetMapping("/articleList")
+    @ApiOperation(value = "分页查询博文列表",notes = "分页查询博文列表")
+    public RespVo articleList(){
+        return null;
+    }
+
+    @GetMapping("/hotArticleList")
+    @ApiOperation(value = "查询热门文章列表",notes = "查询热门文章列表")
+    public RespVo hotArticleList(){
+        return null;
+    }
+
+    @PutMapping("/updateViewCount/{id}")
+    @ApiOperation(value = "增加阅读数",notes = "增加阅读数")
+    public RespVo updateViewCount(@PathVariable(name = "id") String id){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "查询文章详情",notes = "查询文章详情")
+    public RespVo queryArticleInfo(@PathVariable(name = "id") String id){
+        return null;
+    }
+
 }
